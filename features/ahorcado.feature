e58035f2ab8bd5be158a60cfb504aa55d3012497
Damián Ciancio <damianciancio7@gmail.com>
2020-07-03 20:01:39 -0300

    Agrego BDD tests.

--- features/ahorcado.feature
+++ features/ahorcado.feature
@@ -1,20 +1,20 @@
-Feature: Ahorcado functionality
-    Scenario: letter input
-    Given a new hanged game starting with hello word 
-    When i input "p"
-    Then i get an error message
+Feature: Funcionalidad de Ahorcado
+    Scenario: Ingreso de letra incorrecta
+    Given el comienzo de un nuevo juego con la palabra hola
+    When ingreso p
+    Then obtengo un mensaje de error
 
-    Scenario: word input
-    Given a new hanged game starting with hello word 
-    When i input "hola"
-    Then i will be redirect to the winning page
-    
-    Scenario: number of attempts
-    Given a new hanged game starting with hello word 
-    When i input "r"
-    Then i will have "6" number of attempts remaining
+    Scenario: Ingreso de palabra
+    Given el comienzo de un nuevo juego con la palabra hola
+    When ingreso hola
+    Then gano la partida
 
-    Scenario: testing hanged game browser
-    Given a new hanged game
-    When i enter the browser
-    Then Then i should have a title "Ahorcado" 
+    Scenario: Ingreso letra correcta
+    Given el comienzo de un nuevo juego con la palabra hola
+    When ingreso o
+    Then obtengo un mensaje de acierto
+
+    Scenario: Intentos restantes
+    Given el comienzo de un nuevo juego con la palabra hola
+    When ingreso p
+    Then me quedan 6 intentos restantes
---
+++ tests/test_web.py
@@ -0,0 +1,51 @@
+import pytest
+
+from pytest_bdd import scenarios, given, when,then, parsers
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+
+scenarios('../features/ahorcado.feature')
+
+@pytest.fixture
+def browser():
+    # Initialize ChromeDriver
+    driver = Chrome()
+
+    # Wait implicitly for elements to be ready before attempting interactions
+    driver.implicitly_wait(10)
+
+    # Return the driver object at the end of setup
+    yield driver
+
+    # For cleanup, quit the driver
+    driver.quit()
+
+@given(parsers.parse('el comienzo de un nuevo juego con la palabra hola'))
+def home(browser):
+    browser.get('http://localhost:5000')
+    browser.find_element_by_id('iniciar-link').click()
+
+@when(parsers.parse("ingreso {letter}"))
+def input_letter(browser, letter):
+    input = browser.find_element_by_id('input-letra')
+    input.send_keys(letter + Keys.RETURN)
+
+@then('obtengo un mensaje de error')
+def error_message(browser):
+    result_message = browser.find_element_by_id('result-message')
+    assert result_message.text == 'Error'
+
+@then('obtengo un mensaje de acierto')
+def ok_message(browser):
+    result_message = browser.find_element_by_id('result-message')
+    assert result_message.text == 'Muy bien!'
+
+@then('gano la partida')
+def won_game(browser):
+    end_message = browser.find_element_by_id('won-text').text
+    assert end_message == 'Ganaste'
+
+@then(parsers.parse('me quedan {quantity_remaining} intentos restantes'))
+def remaining_attempts(browser, quantity_remaining):
+    end_message = browser.find_element_by_id('remaining-attempts').text
+    assert end_message == quantity_remaining
